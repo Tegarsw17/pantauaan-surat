af9d87a17c1a46434723bad12158794fa60458c0
yasinqurni <yalqurni@gmail.com>
2023-04-04 15:55:30 +0100

    belum kelar

--- BE/app/decorators/surats-decorator.js
+++ BE/app/decorators/surats-decorator.js
@@ -3,26 +3,35 @@ const suratDecorator = (payload) => {
         surat_id: payload.id
     }
 }
-const suratArrayDecorator = async (surat) => {
-    const mappedSurat = surat.map((data)=>{
-        return {
-          id_Surat : data.id,
-          name_item: data.name_item,
-          category_id: data.category_id,
-          category_name: data.category.category_name,
-          description: data.item_description,
-          price: data.price,
-          quantity: data.quantity,
-          images: data.images.map((image) => {
-            return {
-              image_id: image.id,
-              url: image.url
-            }
-          }),
-        }
-      })
-      return await Promise.all(mappedSurat)
-}
+// const suratArrayDecorator = async (surat) => {
+//     const mappedSurat = surat.map((data)=>{
+//         return {
+//             id: data.id,
+//             admin:
+//             "id": 1,
+//             "user_id": 1,
+//             "jenis_surat": "penting",
+//             "nomor_agenda": 2,
+//             "nomor_surat": "21",
+//             "pengirim": "Pelabuhan Perikanan",
+//             "perihal": "permintaan Ijin",
+//             "tanggal_surat": "2022-03-21T00:00:00.000Z",
+//             "tanggal_diterima": "2022-03-21T00:00:00.000Z",
+//             "status": "diterima",
+//             "control": "",
+//             "unit_proses": "APB",
+//             "tindak_lanjut": "segera",
+//             "keterangan": "aman",
+//           images: data.images.map((image) => {
+//             return {
+//               image_id: image.id,
+//               url: image.url
+//             }
+//           }),
+//         }
+//       })
+//       return await Promise.all(mappedSurat)
+// }
 module.exports = {
     suratDecorator
 }
