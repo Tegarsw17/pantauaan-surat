0bc037582aa99fb5d9a266b6e7b2b4af4bd1afe4
yasinqurni <yalqurni@gmail.com>
2023-04-07 03:33:32 +0100

    add swagger

--- BE/app.js
+++ BE/app.js
@@ -2,6 +2,24 @@ const express = require('express')
 const app = express()
 const {userRouter, letterRouter, uploadRouter, approvalRouter } = require('./app/routers')
 const bodyParser = require('body-parser')
+const { cors, corsOptions } = require('./app/middlewares/cors')
+const swaggerUi = require(`swagger-ui-express`)
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load(`./swagger.yaml`);
+
+
+
+app.use(cors(corsOptions))
+
+app.use(function(req, res, next) {
+    res.header(
+        "Access-Control-Allow-Origin: *",
+        'Access-Control-Allow-Headers',
+        'authorization, Origin, Content-Type, Accept'
+    )
+
+    next()
+})
 
 app.use(bodyParser.json())
 app.use(express.urlencoded({extended: true}))
@@ -10,6 +28,7 @@ app.use('/v1', userRouter)
 app.use('/v1', letterRouter)
 app.use('/v1', uploadRouter)
 app.use('/v1', approvalRouter)
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 // app.use('/v1', imageRouter)
 
 
