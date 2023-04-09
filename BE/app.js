1f1204d35994c99a0b2ce95ed48e7727bea10834
Tegarsw17 <tegarsatriyawiguna99@gmail.com>
2023-04-09 19:56:23 +0700

    register api fetch

--- BE/app.js
+++ BE/app.js
@@ -1,28 +1,32 @@
 const express = require('express')
 const app = express()
-const {userRouter, letterRouter, uploadRouter, approvalRouter, roleRouter } = require('./app/routers')
+const {
+  userRouter,
+  letterRouter,
+  uploadRouter,
+  approvalRouter,
+  roleRouter,
+} = require('./app/routers')
 const bodyParser = require('body-parser')
-// const { cors, corsOptions } = require('./app/middlewares/cors')
+const { cors, corsOptions } = require('./app/middlewares/cors')
 const swaggerUi = require(`swagger-ui-express`)
-const YAML = require('yamljs');
-const swaggerDocument = YAML.load('./swagger.yaml');
+const YAML = require('yamljs')
+const swaggerDocument = YAML.load('./swagger.yaml')
 
+app.use(cors())
 
+app.use(function (req, res, next) {
+  res.header(
+    'Access-Control-Allow-Origin: *',
+    'Access-Control-Allow-Headers',
+    'authorization, Origin, Content-Type, Accept'
+  )
 
-// app.use(cors(corsOptions))
-
-// app.use(function(req, res, next) {
-//     res.header(
-//         "Access-Control-Allow-Origin: *",
-//         'Access-Control-Allow-Headers',
-//         'authorization, Origin, Content-Type, Accept'
-//     )
-
-//     next()
-// })
+  next()
+})
 
 app.use(bodyParser.json())
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }))
 
 app.use('/v1', userRouter)
 app.use('/v1', letterRouter)
@@ -31,6 +35,5 @@ app.use('/v1', approvalRouter)
 app.use('/v1', roleRouter)
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 // app.use('/v1', imageRouter)
-
 
 module.exports = app
