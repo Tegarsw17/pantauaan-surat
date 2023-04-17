5058bc733a0d544a544de29a7fe00a077de73438
Tegarsw17 <tegarsatriyawiguna99@gmail.com>
2023-04-17 15:10:57 +0700

    bug fix

--- FE/dist/js/navbar.js
+++ FE/dist/js/navbar.js
@@ -96,7 +96,7 @@ with font-awesome or any other icon font library -->
   </a>
   <ul class="nav nav-treeview">
     <li class="nav-item">
-      <a href="./suratmasuk.html" class="nav-link">
+      <a href="./suratmasukmanager.html" class="nav-link">
         <i class="far fa-circle nav-icon"></i>
         <p>Surat Masuk</p>
       </a>
